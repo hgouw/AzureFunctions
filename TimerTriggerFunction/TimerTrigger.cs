0805d905b2628fd93134f8e22b73690664798da1
Herman Gouw <hermangouw@gmail.com>
2017-11-08 14:44:09 +1100

    Set the timer to be triggered daily at 12am

--- TimerTriggerFunction/TimerTrigger.cs
+++ TimerTriggerFunction/TimerTrigger.cs
@@ -7,7 +7,8 @@ namespace TimerTriggerFunction
     public static class TimerTrigger
     {
         [FunctionName("TimerTrigger")]
-        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
+        // Set the timer to be triggered daily at 12am
+        public static void Run([TimerTrigger("0 0 0 */1 * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
