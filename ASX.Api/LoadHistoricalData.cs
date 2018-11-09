c45b3d0aa50d300c78b3001054aae072f3e7f6ca
Herman Gouw <hermangouw@gmail.com>
2018-11-09 13:45:16 +1100

    More work on LoadHistoricalData

--- ASX.Api/LoadHistoricalData.cs
+++ ASX.Api/LoadHistoricalData.cs
@@ -7,8 +7,16 @@ namespace ASX.Api
     public static class LoadHistoricalData
     {
         [FunctionName("LoadHistoricalData")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        // "*/5 * * * * *" - every 5 seconds
+        // "0 */1 * * * *" - every minute
+        public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, TraceWriter log)
         {
+            var url = "/assets/data/week20180615.zip";
+
+            if (myTimer.IsPastDue)
+            {
+                log.Info("Timer is running late!");
+            }
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
